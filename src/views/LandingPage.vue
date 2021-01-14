49eb34a26cea331894fb64d24b00eae69dc317b5
Haunted Bees <fench@hauntedbees.com>
2021-01-13 21:55:00 -0800

    added a landing page

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,55 @@
+<template>
+<v-container>
+	<v-sheet shaped class="pa-5">
+		<h2>The Haunted Bees Food Portal</h2>
+        <p class="px-6">
+            Hey there! Welcome to the Food Portal! For some reason I do a lot of food things now, I guess. Three food things, to be precise,
+            so click below to check one of them out! This page on its own isn't very interesting.
+        </p>
+        <v-row>
+            <v-col cols="12" md="4">
+                <router-link to="/world" class="no-line">
+                    <v-card class="pa-5" elevation="3" color="secondary">
+                        <h3>Areund the World</h3>
+                        <p>
+                            Take a trip with me around the world! Not actually, but through the magic of me cooking food and listening to music 
+                            from around the world, and the magic of you reading about it, looking at pictures of it, and listening to some of 
+                            that music, too!
+                        </p>
+                    </v-card>
+                </router-link>
+            </v-col>
+            <v-col cols="12" md="4">
+                <router-link to="/vegan" class="no-line">
+                    <v-card class="pa-5" elevation="3" color="secondary">
+                        <h3>Alt. Food Reviews</h3>
+                        <p>
+                            Like the taste of meat or milk or what-have-you? Want to eat less of it for whatever reason? Not sure what imitation 
+                            products are worth trying out? Then look no further, because I'm drinking all the gross nut milks so you don't have to!
+                        </p>
+                    </v-card>
+                </router-link>
+            </v-col>
+            <v-col cols="12" md="4">
+                <router-link to="/spice" class="no-line">
+                    <v-card class="pa-5" elevation="3" color="secondary">
+                        <h3>Spiceapedia</h3>
+                        <p>
+                            Am I qualified to tell you about how to use spices and herbs to their fullest potentials? Of course not! Does the fact
+                            that I spent a good amount of time doing research on this subject? Still probably not, but I'm sharing the results of that research anyway!
+                        </p>
+                    </v-card>
+                </router-link>
+            </v-col>
+        </v-row>
+        <p class="px-6">
+            Also if you want to see like, privacy policy stuff or whatever, <router-link to="/abeut">we've got that, too</router-link>.
+        </p>
+	</v-sheet>
+</v-container>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+@Component
+export default class LandingPage extends Vue { }
+</script>
--- src/views/views.module.ts
+++ src/views/views.module.ts
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
+import LandingPage from "src/views/LandingPage.vue";
 import WorldPage from "src/views/WorldPage.vue";
 import AltPage from "src/views/AltPage.vue";
 import SpicePage from "src/views/SpicePage.vue";
+import Abeut from "src/views/Abeut.vue";
 import WorldRoutes from "src/views/world/world.module";
 import AltRoutes from "src/views/alt/alt.module";
 import SpiceRoutes from "src/views/spice/spice.module";
@@ -10,7 +12,10 @@ Vue.use(VueRouter);
 const routes:Array<RouteConfig> = [
     { path: "/world", component: WorldPage, meta: { title: "Areund the World" }, children: WorldRoutes },
     { path: "/vegan", component: AltPage, meta: { title: "Alt. Food Reviews" }, children: AltRoutes },
-    { path: "/spice", component: SpicePage, meta: { title: "Spiceapedia" }, children: SpiceRoutes }
+    { path: "/spice", component: SpicePage, meta: { title: "Spiceapedia" }, children: SpiceRoutes },
+    { path: "/abeut", component: Abeut, meta: { title: "Food Portal" } },
+    { path: "/", component: LandingPage, meta: { title: "Food Portal" } },
+    { path: "/*", redirect: "/" }
 ];
 const router = new VueRouter({ mode: "history", base: process.env.BASE_URL, routes, scrollBehavior() { return { x: 0, y: 0 }; } });
 router.afterEach(to => {
