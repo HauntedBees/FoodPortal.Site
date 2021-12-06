b502101981d064694028645a7b8291700277527b
Haunted Bees <fench@hauntedbees.com>
2021-12-05 17:31:07 -0800

    librejs fix

--- vue.config.js
+++ vue.config.js
@@ -16,30 +16,27 @@ module.exports = {
       }
     },
     chainWebpack: config => {
-      config.plugin("banner").use(BannerPlugin, [{ banner: `/**
-*
-* @source: https://github.com/HauntedBees/FoodPortal.Site
-*
-* @licstart The following is the entire license notice for the JavaScript code in this page.
-*
-* Copyright (C) 2020 Sean Finch
-*
-* The JavaScript code in this page is free software: you can
-* redistribute it and/or modify it under the terms of the GNU
-* Affero General Public License (GNU AGPL) as published by the Free Software
-* Foundation, either version 3 of the License, or (at your option)
-* any later version.  The code is distributed WITHOUT ANY WARRANTY;
-* without even the implied warranty of MERCHANTABILITY or FITNESS
-* FOR A PARTICULAR PURPOSE.  See the GNU AGPL for more details.
-*
-* As additional permission under GNU AGPL version 3 section 7, you
-* may distribute non-source (e.g., minimized or compacted) forms of
-* that code without the copy of the GNU AGPL normally required by
-* section 4, provided you include this license notice and a URL
-* through which recipients can access the Corresponding Source.
-*
-* @licend  The above is the entire license notice for the JavaScript code in this page.
-*/
+      config.plugin("banner").use(BannerPlugin, [{ banner: `@source: https://github.com/HauntedBees/FoodPortal.Site/
+
+@licstart  The following is the entire license notice for the
+JavaScript code in this page.
+
+Copyright (C) Sean Finch
+
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this page.
+
 ` }])
     },
     transpileDependencies: [ "vuetify" ],
